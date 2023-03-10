338a30d125b3793f91144f6e9cea8839dc85c779
gastonloco <ferreyragaston351@gmail.com>
2023-03-10 01:41:01 -0300

    grid Nª2

--- home.php
+++ home.php
@@ -20,10 +20,10 @@ if(isset($_POST['add_to_cart'])){
    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
    if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
+      $message[] = 'ya agregado al carrito!';
    }else{
       mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
+      $message[] = 'producto añadido al carrito!';
    }
 
 }
@@ -80,12 +80,12 @@ if(isset($_POST['add_to_cart'])){
             </div>
             <div class="price">$<?php echo $fetch_products['price']; ?>/-
             </div>
-            <input type="number" min="1" name="product_quantity" value="1" class="qty">
+            <input class="qty" type="number" min="1" name="product_quantity" value="1" class="qty">
             <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
             <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
             <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
          </div>
-         
+
          <div class="booton">
             <input type="submit" value="añadir al carrito" name="add_to_cart" class="btn">
          </div>
--- shop.php
+++ shop.php
@@ -20,10 +20,10 @@ if(isset($_POST['add_to_cart'])){
    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
    if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
+      $message[] = 'ya agregado al carrito!';
    }else{
       mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
+      $message[] = 'producto añadido al carrito!';
    }
 
 }
@@ -66,14 +66,21 @@ if(isset($_POST['add_to_cart'])){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
      <form action="" method="post" class="box">
+      <div class="imagen">
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+      </div>
+      <div class="info">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
       <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
       <input type="number" min="1" name="product_quantity" value="1" class="qty">
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+      </div>
+      <div class="booton">
       <input type="submit" value="agregar al carrito" name="add_to_cart" class="btn">
+      </div>
+      
      </form>
       <?php
          }
